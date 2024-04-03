c80e2c00f2818b725eba1dd2085ad6e3dc4b2172
Dimityr <dzambazov1986@gmail.com>
2024-04-03 20:26:48 +0300

    Add project files.

---
+++ PersonInfo/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class PersonInfo
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public PersonInfo(string firstName, string lastName, int age)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+    }
+}
---
+++ PersonInfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int lines = int.Parse(Console.ReadLine()!);
+
+List<PersonInfo> persons = new List<PersonInfo>();
+for (int i = 0; i < lines; i++)
+{
+    string[] cmd = Console.ReadLine()!.Split();
+    Person person = new(cmd[0], cmd[1], int.Parse(cmd[2]));
+    persons.Add(person);
+}
+
+persons
+    .OrderBy(p => p.FirstName)
+    .ThenBy(p => p.Age)
+    .ToList()
+    .ForEach(Console.WriteLine);
